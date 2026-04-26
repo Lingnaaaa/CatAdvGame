2c3c853d8b05b11fc4f0a158bea6e9f7cefca6d1
Lingnaaaa <lingnah794@deltalearns.ca>
2026-04-25 19:25:20 -0700

    111

---
+++ CatAdgame/Assets/Scenes/Win.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using TMPro; // TextMeshPro (I used it in the UI so I need it in this line)
+
+public class Win : MonoBehaviour
+{
+    public GameObject winPanel;
+
+    // When this player touch something is trigger
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //If the object's tag is Fish
+        if (other.CompareTag("Fish"))
+        {
+            Debug.Log("Win!");
+            
+            // Appear the "You Win" page
+            winPanel.SetActive(true);
+            
+            // 2. disappear the fish
+            Destroy(other.gameObject);
+            
+        }
+    }
+}
---
+++ CatAdgame/Assets/Scenes/WoodenBox.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class WoodenBox : MonoBehaviour
+{
+    // this code runs when the box is hit
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        // If is the player (Cat) crash the box
+        if (other.gameObject.tag == "Player")
+        {
+            // If player's position is under the box
+            if (other.transform.position.y < transform.position.y)
+            {
+                Debug.Log("<size=30>BOOM!</size>");
+                
+                // destroy the box
+                Destroy(gameObject);
+            }
+        }
+    }
+}
